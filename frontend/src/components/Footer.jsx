7c974f0cbb5ab335b40f1cc644799f78ccf4a54a
Krish Satasiya <85281814+MrKrishSatasiya@users.noreply.github.com>
2025-03-10 18:24:46 +0530

    YEAR

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ Mr.Krish Satasiya - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2025 @ Mr.Krish Satasiya - All Right Reserved.</p>
       </div>
 
     </div>
